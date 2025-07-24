e1c54baaf5cefe5398eb32b6eb1eb4ff81a78f3b
ArezooMrd <moradi.arezoo1992@gmail.com>
2025-07-24 17:28:51 +0330

    fix: fix build issue

--- src/app/(auth)/login/api/index.ts
+++ src/app/(auth)/login/api/index.ts
@@ -6,30 +6,29 @@ import { setCookie } from "@/lib/cookies";
 import { useRouter } from "next/navigation";
 
 export const postLogin = async (request: LoginRequest) => {
- const data = await fetchInstance<LoginResponse>({
-  path: LOGIN,
-  options: {
-   method: "POST",
-   body: JSON.stringify(request),
-  },
-  isFormData: true,
- });
+    const data = await fetchInstance<LoginResponse>({
+        path: LOGIN,
+        options: {
+            method: "POST",
+            body: JSON.stringify(request),
+        },
+    });
 
- return data;
+    return data;
 };
 
 export const useLoginMutation = () => {
- const router = useRouter();
- return useMutation({
-  mutationFn: postLogin,
-  mutationKey: ["login"],
-  onSuccess: (data) => {
-   if (data) {
-    setCookie("token", data.data.access.token);
-    setCookie("user_info", JSON.stringify(data.data.user));
+    const router = useRouter();
+    return useMutation({
+        mutationFn: postLogin,
+        mutationKey: ["login"],
+        onSuccess: (data) => {
+            if (data) {
+                setCookie("token", data.data.access.token);
+                setCookie("user_info", JSON.stringify(data.data.user));
 
-    router.push("/dashboard");
-   }
-  },
- });
+                router.push("/dashboard");
+            }
+        },
+    });
 };
