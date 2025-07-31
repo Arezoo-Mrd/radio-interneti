b5a7b47eb4f61209d24e3bb89e5e2a44e5ba1267
ArezooMrd <moradi.arezoo1992@gmail.com>
2025-07-31 22:22:19 +0330

    fix: add loading bar

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,8 +1,10 @@
 import { AppSidebar } from "@/components/AppSidebar";
 import TopMenu from "@/components/TopMenu";
+import { Progress } from "@/components/ui/progress";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
+import { Suspense } from "react";
 
 export default async function ProtectedLayout({
     children,
@@ -17,15 +19,17 @@ export default async function ProtectedLayout({
 
     return (
         <SidebarProvider className="flex justify-between gap-6 bg-main-background">
-            <AppSidebar />
-            <main className="w-full  p-6">
-                <div className=" flex flex-col gap-6">
-                    <nav className="bg-white w-full h-[62px] rounded-md md:px-4  md:py-5">
-                        <TopMenu />
-                    </nav>
-                    <div className="bg-white w-full h-full rounded-md ">{children}</div>
-                </div>
-            </main>
+            <Suspense fallback={<Progress value={33} className="w-full" />}>
+                <AppSidebar />
+                <main className="w-full  p-6">
+                    <div className=" flex flex-col gap-6">
+                        <nav className="bg-white w-full h-[62px] rounded-md md:px-4  md:py-5">
+                            <TopMenu />
+                        </nav>
+                        <div className="bg-white w-full h-full rounded-md ">{children}</div>
+                    </div>
+                </main>
+            </Suspense>
         </SidebarProvider>
     );
 }
