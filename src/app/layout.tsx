edf4869e96d8e81abb30f38176d9476062be1d63
ArezooMrd <moradi.arezoo1992@gmail.com>
2025-08-02 09:45:34 +0330

    add meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,21 +4,24 @@ import { Toaster } from "@/components/ui/sonner";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
- title: "رادیو اینترنتی",
- description: "رادیو اینترنتی",
+    title: "رادیو اینترنتی",
+    description: "رادیو اینترنتی",
 };
 
 export default function RootLayout({
- children,
+    children,
 }: Readonly<{
- children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
- return (
-  <html lang="fa" dir="rtl">
-   <body className={`font-Peyda`}>
-    <QueryProvider>{children}</QueryProvider>
-    <Toaster className="font-Peyda!" position="top-center" />
-   </body>
-  </html>
- );
+    return (
+        <html lang="fa" dir="rtl">
+            <head>
+                <meta name="viewport" content="width=1200, initial-scale=1" />
+            </head>
+            <body className={`font-Peyda`}>
+                <QueryProvider>{children}</QueryProvider>
+                <Toaster className="font-Peyda!" position="top-center" />
+            </body>
+        </html>
+    );
 }
